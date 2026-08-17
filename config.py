f6d6d481ed8dccaf313d9d507ae8dc5d03a2a329
TeoMixx <leejunjie05202006@gmail.com>
2026-08-17 09:25:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.cnnblhtn5rcb.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "bait3273"
 customdb = "employee"
 custombucket = "leejunjie-employee123"
 customregion = "us-east-1"
